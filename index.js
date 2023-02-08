2e0015e774402bb3afc4c4c76f4493ee3413de88
ruthyi <jbruthlizcano@gmail.com>
2023-02-08 11:46:02 -0500

    change in index.js

--- index.js
+++ index.js
@@ -1,11 +1,13 @@
 const express = require('express');
-const app= express()
-const port=3000
+const app= express();
+const port=3000;
 // req trae del usuario
 // res reponde al usuario
-app.get("/", (req, res )=> {
-  res.send("Hola Mundo  Somos Manuel, Ruth y Ximena");
-})
+const todoRoutes= require("./routes/todoRoutes");
+app.use("/todos",todoRoutes );
+// app.get("/", (req, res )=> {
+//   res.send("Hola Mundo  Somos Manuel, Ruth y Ximena");
+// })
 
 app.listen(port, ()=>{
   console.log("mi puerto es: "+port);
